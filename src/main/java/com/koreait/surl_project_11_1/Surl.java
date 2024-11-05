ef58a1551815f3ea1a2e763cd7ea182524221e1e
ljh01067 <leejh01067@gmail.com>
2024-11-05 14:42:18 +0900

    단축된 URL을 통해 원본 URL로 이동

--- src/main/java/com/koreait/surl_project_11_1/Surl.java
+++ src/main/java/com/koreait/surl_project_11_1/Surl.java
@@ -1,7 +1,10 @@
 package com.koreait.surl_project_11_1;
+
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
 import java.time.LocalDateTime;
 @Getter
 @Setter
@@ -14,4 +17,9 @@ public class Surl {
     private LocalDateTime modifyDate = LocalDateTime.now();
     private String body;
     private String url;
+    @Setter(AccessLevel.NONE)
+    private long count;
+    public void increaseCount() {
+        count++;
+    }
 }
