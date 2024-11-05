5dcdb9cddfec29c6b0458cb3c9cfdb9f1a20d6e3
ljh01067 <leejh01067@gmail.com>
2024-11-05 13:55:14 +0900

    프로젝트 세팅 및 URL추가

---
+++ src/main/java/com/koreait/surl_project_11_1/SurlProject111Application.java
@@ -0,0 +1,9 @@
+package com.koreait.surl_project_11_1;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SurlProject111Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SurlProject111Application.class, args);
+    }
+}
---
+++ src/test/java/com/koreait/surl_project_11_1/SurlProject111ApplicationTests.java
@@ -0,0 +1,9 @@
+package com.koreait.surl_project_11_1;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class SurlProject111ApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
