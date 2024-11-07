d92a41aa89154aad4d432deaa8bb146f6856f73d
ljh01067 <leejh01067@gmail.com>
2024-11-07 11:48:32 +0900

    Github Action Test

---
+++ src/main/java/com/koreait/surl_project_11_1/HomeController.java
@@ -0,0 +1,26 @@
+package com.koreait.surl_project_11_1;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @Value("${custom.site.name}")
+    private String customSiteName;
+
+    @Value("${custom.secret.key}")
+    private String secretKey;
+
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "123Main on " + customSiteName;
+    }
+    @GetMapping("/secretKey")
+    @ResponseBody
+    public String ShowSecretKey() {
+        return "secretKey : " + secretKey;
+    }
+}
