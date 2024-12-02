b375e0b8bfb3c8fc3829dcadcf27195007355122
ljh01067 <leejh01067@gmail.com>
2024-12-02 15:30:18 +0900

    레이아웃

--- front/my-app/src/routes/+layout.svelte
+++ front/my-app/src/routes/+layout.svelte
@@ -1,6 +1,12 @@
-<script lang="ts">
-	import '../app.css';
-	let { children } = $props();
-</script>
-
-{@render children()}
+<header>
+<nav>
+	<a href="/">메인</a>
+	<a href="/about">설명</a>
+</nav>
+</header>
+<slot></slot>
+<footer>
+<nav>
+	<a href="/home/links">링크들</a>
+</nav>
+</footer>
